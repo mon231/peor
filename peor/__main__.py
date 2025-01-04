c602f5e6e3f0c450c35caf288a50759c101174c3
Ariel Tubul <avarti765@gmail.com>
2025-01-05 00:43:52 +0200

    WIP adding 32bit shellcodifier

--- peor/__main__.py
+++ peor/__main__.py
@@ -1,6 +1,24 @@
-import pefile
+import shutil
 import argparse
+from pefile import PE
 from pathlib import Path
+
+
+def dump_memory_layout(pe: PE, output_file: Path):
+    size_of_image = pe.OPTIONAL_HEADER.SizeOfImage
+    ram_layout = bytearray(size_of_image)
+
+    headers_size = pe.OPTIONAL_HEADER.SizeOfHeaders
+    ram_layout[:headers_size] = pe.__data__[:headers_size]
+
+    for section in pe.sections:
+        raw_data = section.get_data()
+        virtual_address = section.VirtualAddress
+        size = min(len(raw_data), section.Misc_VirtualSize)
+        ram_layout[virtual_address : (virtual_address + size)] = raw_data
+
+    # TODO: get per arch, embedded resource
+    output_file.write_bytes(Path('./relocations_resolver32').read_bytes() + ram_layout)
 
 
 def parse_arguments():
@@ -14,7 +32,9 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    raise NotImplementedError('Tool is in WIP state')
+
+    pe = PE(args.input_file)
+    dump_memory_layout(pe, args.output_file)
 
 
 if __name__ == '__main__':
--- relocations_resolver32.asm
+++ relocations_resolver32.asm
@@ -8,7 +8,7 @@ get_eip:
 
 find_pe_address:
     mov edi, ebx               ; Store EIP in EDI
-    add edi, address_of_pe - address_of_shellcode
+    add edi, address_of_pe - get_eip
 
 validate_pe_header:
     cmp word [edi], 0x5A4D     ; Look for MZ signature
@@ -20,7 +20,7 @@ skip_dos_header:
     add edi, eax               ; Point to PE header
 
 process_relocs:
-    mov esi, [edi + 0xB0]      ; Get relocation table RVA
+    mov esi, [edi + 0xA0]      ; Get relocation table RVA
 
     test esi, esi              ; Check if there is a relocation table
     jz pe_entry_jumper
@@ -28,24 +28,38 @@ process_relocs:
     add esi, ebx               ; Get VA of reloc table
 
 reloc_block:
+    mov ecx, [esi]             ; Get block RVA
+
+    test ecx, ecx              ; Check if this is the last block
+    jz pe_entry_jumper         ; If RVA is 0, we're done with all blocks
+
     mov ecx, [esi + 4]         ; Get block size
+    push esi                   ; Save current block pointer
     add esi, 8                 ; Skip to first entry
     sub ecx, 8                 ; Adjust count
 
 next_reloc:
     movzx eax, word [esi]      ; Get relocation entry
-    cmp ax, 0                  ; Check if done
-    je pe_entry_jumper         ; Jump to entry point if done
+    mov edx, eax               ; Save full entry
+    shr edx, 12                ; Get relocation type
+    cmp edx, 3                 ; Check if IMAGE_REL_BASED_HIGHLOW
+    jne skip_reloc
 
     and eax, 0x0FFF           ; Mask to get offset
+    add eax, [esp]            ; Get block RVA
     add eax, ebx              ; Add base address
     mov edx, [eax]            ; Get value to fix
     add edx, ebx              ; Add delta
     mov [eax], edx            ; Write fixed-up value
 
+skip_reloc:
     add esi, 2                ; Next entry
     sub ecx, 2                ; Decrease count
     jnz next_reloc            ; Process next if not done
+
+    pop esi                   ; Restore block pointer
+    add esi, [esi + 4]        ; Move to next block
+    jmp reloc_block           ; Process next block
 
 pe_entry_jumper:
     mov eax, [edi + 0x28]      ; Get the address of the entry point (RVA)
